cc11f1d6c57914ccf37f56252e795962a907ec89
Aaron Obed Giron <aogarrido.g@gmail.com>
2024-08-24 20:29:40 -0600

    Agregar: Boton Flotante

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,27 @@
+import { IconButton } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView, NothingSelectedView } from "../views"
+import { AddOutlined } from "@mui/icons-material"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      {/* <Typography>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore debitis necessitatibus facere alias ut veniam distinctio? Ratione velit nobis a neque necessitatibus, officiis porro voluptates facilis tenetur animi? Aliquid, veritatis?</Typography> */}
-      {/* <NothingSelectedView/> */}
-      <NoteView/>
+      <NothingSelectedView/>
+      {/* <NoteView/> */}
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover':{backgroundColor:'error.main',opacity:0.9},
+          position: 'fixed',
+          right:50,
+          bottom:50
+        }}
+      >
+        <AddOutlined sx={{fontSize:30}}/>    
+      </IconButton>
+
     </JournalLayout>
-    
   )
 }
